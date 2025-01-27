eb67424ba1c83c9ac7ad4682e04bb104f36b44a3
Adam Pruitt <apruitt13@gmail.com>
2025-01-26 20:53:46 -0800

    Fixing minor errors with symbols.

--- CS_Field_App/app/(home)/_layout.tsx
+++ CS_Field_App/app/(home)/_layout.tsx
@@ -2,7 +2,6 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
