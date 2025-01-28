8fea3177e828248a466e9f61b6d001a70c8f2a64
RussNavas <navasr@oregonstate.edu>
2025-01-28 10:46:59 -0600

    added field for directions to db model under the projects entity

--- backend/models.py
+++ backend/models.py
@@ -64,6 +64,8 @@ class Projects(db.Model):
     title = db.Column(db.String(250), nullable=False)
 
     description = db.Column(db.String(250), nullable=False)
+    
+    directions = db.Column(db.Text, nullable=False)
 
     form_definition = db.Column(db.JSON, nullable=False)
 
