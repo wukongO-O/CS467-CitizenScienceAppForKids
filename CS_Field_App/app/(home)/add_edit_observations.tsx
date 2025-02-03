427f5116f7c9a5bec643efab704c21a51f1c4f69
Adam Pruitt <apruitt13@gmail.com>
2025-02-02 19:56:43 -0800

    Adding other two main pages and navigation to them.

---
+++ CS_Field_App/app/(home)/add_edit_observations.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
+
+export default function explore() {
+  return (
+    <ParallaxScrollView
+          headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+          headerImage={
+            <Image
+                source={require('@/assets/images/partial-react-logo.png')}
+                style={styles.reactLogo}
+            />
+          }>
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText type="title">Add or Edit an observation:
+            </ThemedText>
+          </ThemedView>
+    
+          <ThemedView style={styles.stepContainer}>
+            {/* Display the title */}
+            <ThemedText>List of observations that are available to edit are here:</ThemedText>
+          </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+  // Style for the step container
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  // Style for the react logo
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
