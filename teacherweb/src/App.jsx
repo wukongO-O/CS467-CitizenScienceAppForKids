cb09d0dc4aa768ffa3f75d032e6e3a1e6df13310
ramirma2 <ramirma2@oregonstate.edu>
2025-02-03 11:50:16 -0500

    solved merge conflicts

--- teacherweb/src/App.jsx
+++ teacherweb/src/App.jsx
@@ -1,13 +1,26 @@
-import React from "react";
-import AppRoutes from "./routes"; 
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter as Router, Routes, Route} from 'react-router';
+import NavBar from './components/Navbar';
+import Homepage from './pages/Homepage';
+import Projects from './pages/Projects';
+import Account from './pages/Account';
 
 function App() {
   return (
-    <div className="App">
-      <AppRoutes />
+    <div>
+      <Router>
+          <NavBar/>
+          <Routes>
+            <Route path="/" element={<Homepage />}/>
+            <Route path="/projects" element={<Projects />}/>
+            <Route path="/account" element={<Account />}/>
+          </Routes>
+      </Router>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ teacherweb/src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import {NavLink} from "react-router"
+
+const NavBar = () => {
+    return(
+        <div className="side-nav">
+            <div className='logo'>
+                <h1 className='main-header'>CITIZEN SCIENCE</h1>
+                <h3 className='purple-header'> Teacher's Portal</h3>
+            </div>
+
+            <nav>
+                <NavLink to='/'>
+                Home
+                </NavLink>
+                <NavLink to='/projects'>
+                Projects
+                </NavLink>
+                <NavLink to='/account'>
+                Account
+                </NavLink>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
