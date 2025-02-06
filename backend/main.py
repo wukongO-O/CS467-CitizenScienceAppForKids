3160ccf935fe6e752a675d1773cc988f6a995f4d
wukongO-O <l.shenglan@gmail.com>
2025-02-05 22:01:31 -0500

    Fixed issue from merge conflict

--- backend/main.py
+++ backend/main.py
@@ -258,6 +258,17 @@ def get_an_observation(obs_id):
     return jsonify(result)
 
 
+# Delete an observation by id
+@app.route("/observations/<int:obs_id>", methods=["DELETE"])
+def delete_observation(obs_id):
+    observation = Observations.query.filter_by(obs_id=obs_id).first()
+    if observation:
+        db.session.delete(observation)
+        db.session.commit()
+        return jsonify({"message": "Observation deleted successfully!"}), 200
+    return jsonify({"error": "Observation not found"}), 404
+
+
 # Update an observation
 @app.route("/observations/<int:obs_id>", methods=["PUT"])
 def edit_observation(obs_id):
