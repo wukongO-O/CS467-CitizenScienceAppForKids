4013ecd4106b5d424c5af093f280651a3b8c5d07
ramirma2 <ramirma2@oregonstated.edu>
2025-03-09 21:07:50 -0400

    updated submisisons page to display custom form data as well as display data con csv

--- teacherweb/src/hooks/useProjectObservations.jsx
+++ teacherweb/src/hooks/useProjectObservations.jsx
@@ -1,19 +1,23 @@
 import {useState, useEffect} from 'react';
 
-export const useProjectObservations = (project_id, setStateFunc) => {
+export const useProjectObservations = (project_id, setHeadersStateFunc) => {
     const [observations, setObservations] = useState(null);
 
     useEffect( ()=> {
         async function fetchObservations () {
             const response = await fetch(`http://127.0.0.1:5000//observations/project/${project_id}`)
             const data = await response.json();
+            
             setObservations(data);
-            if (setStateFunc){
-                setStateFunc(data);
+            if (setHeadersStateFunc){
+                
+                const headers = Object.keys(data[0].data);
+                
+                setHeadersStateFunc(headers);
             }
         }
         fetchObservations();
-    },[project_id, setStateFunc])
+    },[project_id, setHeadersStateFunc])
 
     return observations
 }
